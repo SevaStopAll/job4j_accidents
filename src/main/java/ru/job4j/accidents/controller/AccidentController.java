f0bbd1eb712af1eb75ed4156024cce6ad1ca9a47
SevaStopAll <Sevastopallrh@yandex.ru>
2023-04-02 15:19:07 +0400

    3. @ModelAttribute. Создание инцидента. [#261013]

---
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -0,0 +1,44 @@
+package ru.job4j.accidents.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.service.AccidentService;
+import ru.job4j.accidents.service.SimpleAccidentService;
+
+@Controller
+@AllArgsConstructor
+public class AccidentController {
+    private final AccidentService accidents;
+    private final SimpleAccidentService accidentService;
+
+    @GetMapping("/createAccident")
+    public String viewCreateAccident() {
+        return "accidents/createAccident";
+    }
+
+    @PostMapping("/saveAccident")
+    public String save(@ModelAttribute Accident accident) {
+        accidents.create(accident);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String formEdit(Model model, @PathVariable int id) {
+        var accidentOptional = accidentService.findById(id);
+        if (accidentOptional.isEmpty()) {
+            model.addAttribute("message", "Такого происшествия не зарегистрировано");
+            return "errors/404";
+        }
+        model.addAttribute("accident", accidentOptional.get());
+        return "/accidents/editAccident";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Accident accident) {
+        accidentService.update(accident);
+        return "redirect:/index";
+    }
+}
